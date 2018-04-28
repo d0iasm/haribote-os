f65afea175bf20e682fcc8c0d8d66bebda9dc039
d0iasm <doiasami1219@gmail.com>
2018-04-28 14:33:05 +0900

    Update screen color to stripe

--- bootpack.c
+++ bootpack.c
@@ -5,12 +5,8 @@ void write_mem8(int addr, int data);
 
 void HariMain(void) {
   // VRAM: 0xa0000 ~ 0xaffff
-  for(int i=0xa0000; i<0xa7fff; i++) {
-    write_mem8(i, 5);
-  }
-
-  for(int i=0xa7fff; i<0xaffff; i++) {
-    write_mem8(i, 10);
+  for(int i=0xa0000; i<0xaffff; i++) {
+    write_mem8(i, i & 0x0f);
   }
 
   for(;;) {
