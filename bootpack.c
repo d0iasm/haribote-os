9de7490a4fdc462579c889e16cca004aaa5f0cb6
d0iasm <doiasami1219@gmail.com>
2018-04-29 12:25:29 +0900

    Add functions to initialize GDT and IDT

--- bootpack.c
+++ bootpack.c
@@ -34,8 +34,9 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 
-
+// Libraries
 int tsprintf(char *str, const char *fmt, ...);
+
 
 // This struct size is 12 bytes
 struct BOOTINFO {
@@ -43,6 +44,30 @@ struct BOOTINFO {
   short scrnx, scrny; // 2 bytes * 2 = 4 bytes
   char *vram; // 1 byte
 };
+
+// Each GDT size is 8 bytes and there are 8192 segmentations (0~8191)
+// GDT is global segment descriptor table
+// Set this desctiption address to GDTR resistor
+struct SEGMENT_DESCRIPTOR {
+  short limit_low, base_low;
+  char base_mid, access_right;
+  char limit_high, base_high;
+};
+
+// IDT size is 8 bytes and there are 256 intteruptions (0~255)
+// IDT is interrupt descriptor table
+struct GATE_DESCRIPTOR {
+  short offset_low, selector;
+  char dw_count, access_right;
+  short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
 
 void hari_main(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
@@ -215,3 +240,49 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
   }
   return;
 }
+
+void init_gdtidt(void) {
+  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+  struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *) 0x0026f800;
+
+  for (int i=0; i<8192; i++) {
+    set_segmdesc(gdt + i, 0, 0, 0);
+  }
+
+  set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+  set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+  load_gdtr(0xffff, 0x00270000);
+
+  for (int i=0; i<256; i++) {
+    set_gatedesc(idt + i, 0, 0, 0);
+  }
+  load_idtr(0x7ff, 0x0026f800);
+
+  return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar) {
+  if (limit > 0xfffff) {
+    ar |= 0x8000; // G_bit = 1
+    limit /= 0x1000;
+  }
+
+  sd->limit_low = limit & 0xffff;
+  sd->base_low = base & 0xffff;
+  sd->base_mid = (base >> 16) & 0xff;
+  sd->access_right = ar & 0xff;
+  sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+  sd->base_high = (base >> 24) & 0xff;
+  return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar) {
+  gd->offset_low = offset & 0xffff;
+  gd->selector = selector;
+  gd->dw_count = (ar >> 8) & 0xff;
+  gd->access_right = ar & 0xff;
+  gd->offset_high = (offset >> 16) & 0xffff;
+  return;
+}
+
+
