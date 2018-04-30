ee12e8f2338df25a690bdebbcbeb85728ee4c929
d0iasm <doiasami1219@gmail.com>
2018-04-30 21:31:59 +0900

    Call PIC initialization

--- bootpack.c
+++ bootpack.c
@@ -7,6 +7,9 @@ void hari_main(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
   extern char hankaku[4096];
 
+  init_gdtidt();
+  init_pic();
+
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
