72358c9b6ffa6c9ce60d9204d5c14134a5e2ea89
d0iasm <doiasami1219@gmail.com>
2018-05-01 17:25:07 +0900

    Update movement of mouse

--- bootpack.c
+++ bootpack.c
@@ -49,6 +49,7 @@ void hari_main(void) {
       } else if (fifo8_status(&mousefifo) != 0) {
         i = fifo8_get(&mousefifo);
         io_sti();
+
         if (mouse_decode(&mdec, i) != 0) {
           tsprintf(s, "[lcr %d %d]", mdec.x, mdec.y);
           if ((mdec.btn & 0x01) != 0) {
@@ -62,6 +63,25 @@ void hari_main(void) {
           }
           boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);
+          mx += mdec.x;
+          my += mdec.y;
+          if (mx < 0) {
+            mx = 0;
+          }
+          if (my < 0) {
+            my = 0;
+          }
+          if (mx > binfo->scrnx - 16) {
+            mx = binfo->scrnx - 16;
+          }
+          if (my > binfo->scrny - 16) {
+            my = binfo->scrny - 16;
+          }
+          tsprintf(s, "(%d, %d)", mx, my);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+          putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
         }
       }
     }
