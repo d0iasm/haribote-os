0456c81ae30e1df980e864a0c8a839473110aa7d
d0iasm <doiasami1219@gmail.com>
2018-05-01 17:19:29 +0900

    Update the way to show mouse click

--- bootpack.c
+++ bootpack.c
@@ -50,11 +50,19 @@ void hari_main(void) {
         i = fifo8_get(&mousefifo);
         io_sti();
         if (mouse_decode(&mdec, i) != 0) {
-          tsprintf(s, "%x %x %x", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+          tsprintf(s, "[lcr %d %d]", mdec.x, mdec.y);
+          if ((mdec.btn & 0x01) != 0) {
+            s[1] = 'L';
+          }
+          if ((mdec.btn & 0x02) != 0) {
+            s[3] = 'R';
+          }
+          if ((mdec.btn & 0x04) != 0) {
+            s[2] = 'C';
+          }
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
         }
-
       }
     }
   }
@@ -96,8 +104,8 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat) {
   }
   if (mdec->phase == 1) {
     if ((dat & 0xc8) == 0x08) {
-    mdec->buf[0] = dat;
-    mdec->phase = 2;
+      mdec->buf[0] = dat;
+      mdec->phase = 2;
     }
     return 0;
   }
