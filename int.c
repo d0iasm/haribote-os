67d1f3357f9119335f458bd30a42c3d519268327
d0iasm <doiasami1219@gmail.com>
2018-05-01 16:34:25 +0900

    Fix to catch mouse data

--- int.c
+++ int.c
@@ -1,4 +1,5 @@
 // int.c: Setting for PIC to send interrupt request
+#include <stdio.h>
 #include "bootpack.h"
 
 
@@ -21,6 +22,8 @@ void init_pic(void) {
 
   io_out8(PIC0_IMR, 0xfb); // 11111011 Deny all except PIC1
   io_out8(PIC1_IMR, 0xff); // 11111111 Deny all interrupt requests
+
+  return;
 }
 
 // Interrupt from PS/2 keyboard
@@ -36,7 +39,7 @@ void inthandler21(int *esp) {
 void inthandler2c(int *esp) {
   unsigned char data;
   io_out8(PIC1_OCW2, 0x64); // Inform the end of IRQ-12's acception to PIC1
-  io_out8(PIC1_OCW2, 0x62); // Inform the end of IRQ-02's acception to PIC0
+  io_out8(PIC0_OCW2, 0x62); // Inform the end of IRQ-02's acception to PIC0
   data = io_in8(PORT_KEYDAT);
   fifo8_put(&mousefifo, data);
   return;
