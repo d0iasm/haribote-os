44022cb8c0131a896665b5cfb950a9a5f60eb80e
d0iasm <doiasami1219@gmail.com>
2018-05-02 10:46:41 +0900

    Add the function to manage a memory

--- bootpack.c
+++ bootpack.c
@@ -13,6 +13,8 @@ void hari_main(void) {
   int mx = (binfo->scrnx - 16) / 2;
   int my = (binfo->scrny - 28 - 16) / 2;
   struct MOUSE_DEC mdec;
+  unsigned int memtotal;
+  struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
   init_gdtidt();
   init_pic();
@@ -26,7 +28,12 @@ void hari_main(void) {
 
   init_keyboard();
   enable_mouse(&mdec);
-  
+
+  memtotal = memtest(0x00400000, 0xbfffffff);
+  memman_init(memman);
+  memman_free(memman, 0x00001000, 0x0009e000); // 0x00001000 - 0x0009efff
+  memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
   init_palette();
   init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(mcursor, COL8_008484);
@@ -36,6 +43,11 @@ void hari_main(void) {
 
   i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
   tsprintf(s, "memory %dMB", i);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+
+
+  tsprintf(s, "memory %dMB   free : %dKB",
+      memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
   for (;;) {
@@ -146,4 +158,84 @@ not_memory:
     *p = old;
   }
   return i;
+}
+
+void memman_init(struct MEMMAN *man) {
+  man->frees = 0;
+  man->maxfrees = 0;
+  man->lostsize = 0;
+  man->losts = 0;
+  return;
+}
+
+unsigned int memman_total(struct MEMMAN *man) {
+  unsigned int t = 0;
+  for (int i=0; i<man->frees; i++) {
+    t += man->free[i].size;
+  }
+  return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, int size) {
+  unsigned int a;
+  for (int i=0; i<man->frees; i++) {
+    if (man->free[i].size >= size) {
+      a = man->free[i].addr;
+      man->free[i].addr += size;
+      man->free[i].size -= size;
+      if (man->free[i].size == 0) {
+        man->frees--;
+        for (; i<man->frees; i++) {
+          man->free[i] = man->free[i+1];
+        }
+      }
+      return a;
+    }
+  }
+  return 0;
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size) {
+  int i, j;
+  for (i=0; i<man->frees; i++) {
+    if (man->free[i].addr > addr) break;
+  }
+
+  if (i > 0) {
+    if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+      man->free[i - 1].size += size;
+      if (i < man->frees) {
+        if (addr + size == man->free[i].addr) {
+          man->free[i - 1].size += man->free[i].size;
+          man->frees--;
+          for (; i < man->frees; i++) {
+            man->free[i] = man->free[i + 1];
+          }
+        }
+      }
+      return 0;
+    }
+  }
+  if (i < man->frees) {
+    if (addr + size == man->free[i].addr) {
+      man->free[i].addr = addr;
+      man->free[i].size += size;
+      return 0;
+    }
+  }
+  if (man->frees < MEMMAN_FREES) {
+    for (j = man->frees; j > i; j--) {
+      man->free[j] = man->free[j - 1];
+    }
+    man->frees++;
+    if (man->maxfrees < man->frees) {
+      man->maxfrees = man->frees;
+    }
+    man->free[i].addr = addr;
+    man->free[i].size = size;
+    return 0;
+  }
+  man->losts++;
+  man->lostsize += size;
+  return -1;
 }
--- bootpack.h
+++ bootpack.h
@@ -6,9 +6,24 @@
 /* -- bootpack.c start -- */
 #define EFLAGS_AC_BIT 0x00040000
 #define CR0_CACHE_DISABLE 0x60000000
+#define MEMMAN_FREES 4090 // About 32kB
+#define MEMMAN_ADDR 0x003c0000
+
+struct FREEINFO {
+  unsigned int addr, size;
+};
+
+struct MEMMAN {
+  int frees, maxfrees, lostsize, losts;
+  struct FREEINFO free[MEMMAN_FREES];
+};
 
 unsigned int memtest(unsigned int start, unsigned int end);
 unsigned int memtest_sub(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 /* -- bootpack.c start -- */
 
 
