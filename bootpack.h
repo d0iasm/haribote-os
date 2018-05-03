cdfba791d9d4aaa510fd1f30bf739b0bc49f1195
d0iasm <doiasami1219@gmail.com>
2018-05-03 09:56:31 +0900

    Reformat struct sheet to add sheet control

--- bootpack.h
+++ bootpack.h
@@ -204,6 +204,7 @@ struct BOOTINFO { // 0x0ff0 ~ 0x0fff
 struct SHEET {
   unsigned char *buf;
   int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+  struct SHTCTL *ctl;
 };
 
 struct SHTCTL { // sheet control
@@ -216,11 +217,11 @@ struct SHTCTL { // sheet control
 struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
 struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
-void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_updown(struct SHEET *sht, int height);
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1);
-void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
-void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
+void sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHEET *sht);
 /* -- sheet.c end --*/
 
 
