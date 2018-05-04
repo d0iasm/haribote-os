5e7a589c90b3a13910b7f9b723a896a5787b34e8
d0iasm <doiasami1219@gmail.com>
2018-05-04 09:54:20 +0900

    Update speeding up of refresh screen

--- bootpack.h
+++ bootpack.h
@@ -219,7 +219,7 @@ struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
 void sheet_updown(struct SHEET *sht, int height);
 void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
-void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1);
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0);
 void sheet_slide(struct SHEET *sht, int vx0, int vy0);
 void sheet_free(struct SHEET *sht);
 /* -- sheet.c end --*/
