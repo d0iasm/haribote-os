8794af7669f16280e8879b8e67205f966172c4a2
d0iasm <doiasami1219@gmail.com>
2018-05-04 09:35:49 +0900

    Add a window by adding a new sheet

--- bootpack.c
+++ bootpack.c
@@ -3,6 +3,8 @@
 #include "bootpack.h"
 
 
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
+
 void hari_main(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], keybuf[32], mousebuf[128];
@@ -11,8 +13,8 @@ void hari_main(void) {
   struct MOUSE_DEC mdec;
   struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
   struct SHTCTL *shtctl;
-  struct SHEET *sht_back, *sht_mouse;
-  unsigned char *buf_back, buf_mouse[256];
+  struct SHEET *sht_back, *sht_mouse, *sht_win;
+  unsigned char *buf_back, buf_mouse[256], *buf_win;
 
   init_gdtidt();
   init_pic();
@@ -31,19 +33,36 @@ void hari_main(void) {
 
   init_palette();
   shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+
   sht_back = sheet_alloc(shtctl);
   sht_mouse = sheet_alloc(shtctl);
+  sht_win = sheet_alloc(shtctl);
+
   buf_back = (unsigned char *)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+  buf_win = (unsigned char *)memman_alloc_4k(memman, 160 * 68);
+
   sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
   sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+  sheet_setbuf(sht_win, buf_win, 160, 68, -1); 
+
   init_screen8(buf_back, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(buf_mouse, 99);
+
+  make_window8(buf_win, 160, 68, "window");
+  putfonts8_asc(buf_win, 160, 24, 28, COL8_000000, "Welcome to");
+  putfonts8_asc(buf_win, 160, 24, 44, COL8_000000, "  Haribote-OS!");
+
   sheet_slide(sht_back, 0, 0);
+
   mx = (binfo->scrnx - 16) / 2;
   my = (binfo->scrny - 28 - 16) / 2;
+
   sheet_slide(sht_mouse, mx, my);
+  sheet_slide(sht_win, 80, 72);
+
   sheet_updown(sht_back,  0);
-  sheet_updown(sht_mouse, 1);
+  sheet_updown(sht_win, 1);
+  sheet_updown(sht_mouse, 2);
 
   tsprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
@@ -106,4 +125,54 @@ void hari_main(void) {
       }
     }
   }
+}
+
+
+
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title) {
+  static char closebtn[14][16] = {
+    "OOOOOOOOOOOOOOO@",
+    "OQQQQQQQQQQQQQ$@",
+    "OQQQQQQQQQQQQQ$@",
+    "OQQQ@@QQQQ@@QQ$@",
+    "OQQQQ@@QQ@@QQQ$@",
+    "OQQQQQ@@@@QQQQ$@",
+    "OQQQQQQ@@QQQQQ$@",
+    "OQQQQQ@@@@QQQQ$@",
+    "OQQQQ@@QQ@@QQQ$@",
+    "OQQQ@@QQQQ@@QQ$@",
+    "OQQQQQQQQQQQQQ$@",
+    "OQQQQQQQQQQQQQ$@",
+    "O$$$$$$$$$$$$$$@",
+    "@@@@@@@@@@@@@@@@"
+  };
+  int x, y;
+  char c;
+  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
+  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+  boxfill8(buf, xsize, COL8_000084, 3, 3, xsize - 4, 20);
+  boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
+  putfonts8_asc(buf, xsize, 24, 4, COL8_FFFFFF, title);
+  for (y = 0; y < 14; y++) {
+    for (x = 0; x < 16; x++) {
+      c = closebtn[y][x];
+      if (c == '@') {
+        c = COL8_000000;
+      } else if (c == '$') {
+        c = COL8_848484;
+      } else if (c == 'Q') {
+        c = COL8_C6C6C6;
+      } else {
+        c = COL8_FFFFFF;
+      }
+      buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+    }
+  }
+  return;
 }
