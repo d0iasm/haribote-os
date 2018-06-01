b943caab6fa324b56a7ff48aed8b77abc4bd3217
d0iasm <doiasami1219@gmail.com>
2018-06-01 23:59:14 +0900

    Update taskswitch

--- bootpack.c
+++ bootpack.c
@@ -16,24 +16,24 @@ struct TSS32 {
 
 void task_b_main(void) {
   struct FIFO32 fifo;
-  struct TIMER *timer;
+  struct TIMER *timer_ts;
   int i, fifobuf[128];
 
   fifo32_init(&fifo, 128, fifobuf);
-  timer = timer_alloc();
-  timer_init(timer, &fifo, 1);
-  timer_settime(timer, 500);
+  timer_ts = timer_alloc();
+  timer_init(timer_ts, &fifo, 1);
+  timer_settime(timer_ts, 2);
 
   for (;;) {
     io_cli();
     if (fifo32_status(&fifo) == 0) {
-      io_sti();
-      io_hlt();
+      io_stihlt();
     } else {
       i = fifo32_get(&fifo);
       io_sti();
       if (i == 1) {
-        farjmp(0, 3*8);
+        farjmp(0, 3 * 8);
+        timer_settime(timer_ts, 2);
       }
     }
   }
@@ -44,7 +44,7 @@ void hari_main(void) {
   struct FIFO32 fifo;
   char s[40];
   int fifobuf[128];
-  struct TIMER *timer, *timer2, *timer3;
+  struct TIMER *timer, *timer2, *timer3, *timer_ts;
   int mx, my, i, cursor_x, cursor_c;
   unsigned int memtotal;
   struct MOUSE_DEC mdec;
@@ -75,6 +75,10 @@ void hari_main(void) {
   timer3 = timer_alloc();
   timer_init(timer3, &fifo, 1);
   timer_settime(timer3, 50);
+
+  timer_ts = timer_alloc();
+  timer_init(timer_ts, &fifo, 2);
+  timer_settime(timer_ts, 2);
 
   memtotal = memtest(0x00400000, 0xbfffffff);
   memman_init(memman);
@@ -146,7 +150,10 @@ void hari_main(void) {
     } else {
       i = fifo32_get(&fifo);
       io_sti();
-      if (256 <= i && i <= 511) { // keyboard data
+      if (i == 2) {
+        farjmp(0, 4*8);
+        timer_settime(timer_ts, 2);
+      } else if (256 <= i && i <= 511) { // keyboard data
         tsprintf(s, "%x", i - 256);
         putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
         if (i < 0x54 + 256) {
@@ -199,7 +206,6 @@ void hari_main(void) {
         }
       } else if (i == 10) {
         putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-        farjmp(0, 4*8);
       } else if (i == 3) {
         putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
       } else if (i <= 1) { 
