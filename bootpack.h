7b4d210f1575465fffd8e5495cbc6c79c03db08c
d0iasm <doiasami1219@gmail.com>
2018-08-04 23:29:02 +0900

    Add multi task idle

--- bootpack.h
+++ bootpack.h
@@ -208,6 +208,7 @@ struct TASK *task_alloc(void);
 void task_run(struct TASK *task, int level, int priority);
 void task_switch(void);
 void task_sleep(struct TASK *task);
+void task_idle(void);
 /* -- mtask.c end -- */
 
 
