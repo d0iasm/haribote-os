0b342354e1aac418078e09576947d2c0e7563135
d0iasm <doiasami1219@gmail.com>
2018-12-14 00:43:39 +0900

    Execute only files having Hari sigature in its header

--- console.c
+++ console.c
@@ -236,6 +236,7 @@ void cmd_cat(struct CONSOLE* cons, int* fat, char* cmdline)
 
 int cmd_app(struct CONSOLE* cons, int* fat, char* cmdline)
 {
+  int segsiz, datsiz, esp, dathrb;
   struct MEMMAN* memman = (struct MEMMAN*)MEMMAN_ADDR;
   struct FILEINFO* finfo;
   struct SEGMENT_DESCRIPTOR* gdt = (struct SEGMENT_DESCRIPTOR*)ADR_GDT;
@@ -265,19 +266,26 @@ int cmd_app(struct CONSOLE* cons, int* fat, char* cmdline)
   if (finfo != 0) {
     // Find the file.
     p = (char*)memman_alloc_4k(memman, finfo->size);
-    q = (char*)memman_alloc_4k(memman, 64 * 1024);
-    *((int*)0xfe8) = (int)p;
     file_loadfile(finfo->clustno, finfo->size, p, fat, (char*)(ADR_DISKIMG + 0x003e00));
-    // 0x60 means this segment only has app segment authority.
-    set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
-    set_segmdesc(gdt + 1004, 64 * 1024 - 1, (int)q, AR_DATA32_RW + 0x60);
-    if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) {
-      start_app(0x1b, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+    if (finfo->size >= 36 && strncmp(p + 4, "Hari", 4) == 0 && *p == 0x00) {
+      segsiz = *((int*)(p + 0x0000));
+      esp = *((int*)(p + 0x000c));
+      datsiz = *((int*)(p + 0x0010));
+      dathrb = *((int*)(p + 0x0014));
+      q = (char*)memman_alloc_4k(memman, segsiz);
+      *((int*)0xfe8) = (int)q;
+      set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER + 0x60);
+      set_segmdesc(gdt + 1004, segsiz - 1,
+          (int)q, AR_DATA32_RW + 0x60);
+      for (i = 0; i < datsiz; i++) {
+        q[esp + i] = p[dathrb + i];
+      }
+      start_app(0x1b, 1003 * 8, esp, 1004 * 8, &(task->tss.esp0));
+      memman_free_4k(memman, (int)q, segsiz);
     } else {
-      start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+      cons_putstr0(cons, "File format error.\n");
     }
     memman_free_4k(memman, (int)p, finfo->size);
-    memman_free_4k(memman, (int)q, 64 * 1024);
     cons_newline(cons);
     return 1;
   }
