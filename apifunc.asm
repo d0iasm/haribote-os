3148ec7b4b08593dc5b946d174712bfe3a7f3300
d0iasm <doiasami1219@gmail.com>
2018-12-17 18:57:00 -0800

    Add new APIs to draw string and rectangle on a window

--- apifunc.asm
+++ apifunc.asm
@@ -3,6 +3,8 @@
   global api_putchar
   global api_putstr0
   global api_openwin
+  global api_boxfilwin
+  global api_putstrwin
   global api_end
 
   section .text
@@ -33,6 +35,44 @@ api_openwin: ; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char 
   mov ecx, [esp + 32] ; title
   int 0x40
   pop ebx
+  pop esi
+  pop edi
+  ret
+
+api_putstrwin: ; void api_putstrwin(int win, int x, int y, int col, int len, char *str);
+  push edi
+  push esi
+  push ebp
+  push ebx
+  mov edx, 6
+  mov ebx, [esp + 20] ; win
+  mov esi, [esp + 24] ; x
+  mov edi, [esp + 28] ; y
+  mov eax, [esp + 32] ; col
+  mov ecx, [esp + 36] ; len
+  mov ebp, [esp + 40] ; str
+  int 0x40
+  pop ebx
+  pop ebp
+  pop esi
+  pop edi
+  ret
+
+api_boxfilwin: ; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
+  push edi
+  push esi
+  push ebp
+  push ebx
+  mov edx, 7
+  mov ebx, [esp + 20] ; win
+  mov eax, [esp + 24] ; x0
+  mov ecx, [esp + 28] ; y0
+  mov esi, [esp + 32] ; x1
+  mov edi, [esp + 36] ; y1
+  mov ebp, [esp + 40] ; col
+  int 0x40
+  pop ebx
+  pop ebp
   pop esi
   pop edi
   ret
