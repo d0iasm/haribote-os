46c08cc3485005e98f5ec601036c30a45ff81863
d0iasm <doiasami1219@gmail.com>
2018-12-18 20:37:12 -0800

    Add an API to refresh a window

--- apifunc.asm
+++ apifunc.asm
@@ -9,6 +9,7 @@
   global api_malloc
   global api_free
   global api_point
+  global api_refreshwin
   global api_end
 
   section .text
@@ -127,6 +128,22 @@ api_point: ; void api_point(int win, int x, int y, int col);
   pop edi
   ret
 
+api_refreshwin: ; void api_refreshwin(int win, int x0, int y0, int x1, int y1);
+  push edi
+  push esi
+  push ebx
+  mov edx, 12
+  mov ebx, [esp+16] ; win
+  mov eax, [esp+20] ; x0
+  mov ecx, [esp+24] ; y0
+  mov esi, [esp+28] ; x1
+  mov edi, [esp+32] ; y1
+  int 0x40
+  pop ebx
+  pop esi
+  pop edi
+  ret
+  
 api_end: ; void api_end(void);
   mov edx, 4
   int 0x40
--- console.c
+++ console.c
@@ -338,13 +338,17 @@ int* hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
     sheet_updown(sht, 3); // The height of 3 means above task_a window.
     reg[7] = (int)sht;
   } else if (edx == 6) {
-    sht = (struct SHEET*)ebx;
+    sht = (struct SHEET*)(ebx & 0xfffffffe);
     putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char*)ebp + ds_base);
-    sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+    if ((ebx & 1) == 0) {
+      sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+    }
   } else if (edx == 7) {
-    sht = (struct SHEET*)ebx;
+    sht = (struct SHEET*)(ebx & 0xfffffffe);
     boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
-    sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+    if ((ebx & 1) == 0) {
+      sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+    }
   } else if (edx == 8) {
     memman_init((struct MEMMAN*)(ebx + ds_base));
     ecx &= 0xfffffff0; // Round up to 16-byte unit.
@@ -356,9 +360,14 @@ int* hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
     ecx = (ecx + 0x0f) & 0xfffffff0; // Round up to 16-byte unit.
     memman_free((struct MEMMAN*)(ebx + ds_base), eax, ecx);
   } else if (edx == 11) {
-    sht = (struct SHEET*)ebx;
+    sht = (struct SHEET*)(ebx & 0xfffffffe);
     sht->buf[sht->bxsize * edi + esi] = eax;
-    sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+    if ((ebx & 1) == 0) {
+      sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+    }
+  } else if (edx == 12) {
+    sht = (struct SHEET*)ebx;
+    sheet_refresh(sht, eax, ecx, esi, edi);
   }
   return 0;
 }
