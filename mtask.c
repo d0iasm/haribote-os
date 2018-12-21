26a440ab233de0db6186cbc55767a244f75fc17e
d0iasm <doiasami1219@gmail.com>
2018-12-21 15:03:28 -0800

    Format

--- mtask.c
+++ mtask.c
@@ -1,41 +1,42 @@
 // mtask.c
-
 #include "bootpack.h"
 
+struct TASKCTL* taskctl;
+struct TIMER* task_timer;
 
-struct TASKCTL *taskctl;
-struct TIMER *task_timer;
-
-
-struct TASK *task_now(void) {
-  struct TASKLEVEL *tl = &taskctl->level[taskctl->now_lv];
+struct TASK* task_now(void)
+{
+  struct TASKLEVEL* tl = &taskctl->level[taskctl->now_lv];
   return tl->tasks[tl->now];
 }
 
-void task_add(struct TASK *task) {
-  struct TASKLEVEL *tl = &taskctl->level[task->level];
+void task_add(struct TASK* task)
+{
+  struct TASKLEVEL* tl = &taskctl->level[task->level];
   tl->tasks[tl->running] = task;
   tl->running++;
-  task->flags = 2; 
+  task->flags = 2;
   return;
 }
 
-void task_remove(struct TASK *task) {
+void task_remove(struct TASK* task)
+{
   int i;
-  struct TASKLEVEL *tl = &taskctl->level[task->level];
+  struct TASKLEVEL* tl = &taskctl->level[task->level];
 
   for (i = 0; i < tl->running; i++) {
-    if (tl->tasks[i] == task) break;
+    if (tl->tasks[i] == task)
+      break;
   }
 
   tl->running--;
   if (i < tl->now) {
-    tl->now--; 
+    tl->now--;
   }
   if (tl->now >= tl->running) {
     tl->now = 0;
   }
-  task->flags = 1; 
+  task->flags = 1;
 
   for (; i < tl->running; i++) {
     tl->tasks[i] = tl->tasks[i + 1];
@@ -44,32 +45,36 @@ void task_remove(struct TASK *task) {
   return;
 }
 
-void task_switchsub(void) {
+void task_switchsub(void)
+{
   int i;
   for (i = 0; i < MAX_TASKLEVELS; i++) {
-    if (taskctl->level[i].running > 0) break;
+    if (taskctl->level[i].running > 0)
+      break;
   }
   taskctl->now_lv = i;
   taskctl->lv_change = 0;
   return;
 }
 
-void task_idle(void) {
+void task_idle(void)
+{
   for (;;) {
     io_hlt();
   }
 }
 
-struct TASK *task_init(struct MEMMAN *memman) {
+struct TASK* task_init(struct MEMMAN* memman)
+{
   int i;
   struct TASK *task, *idle;
-  struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+  struct SEGMENT_DESCRIPTOR* gdt = (struct SEGMENT_DESCRIPTOR*)ADR_GDT;
 
-  taskctl = (struct TASKCTL *) memman_alloc_4k(memman, sizeof (struct TASKCTL));
+  taskctl = (struct TASKCTL*)memman_alloc_4k(memman, sizeof(struct TASKCTL));
   for (i = 0; i < MAX_TASKS; i++) {
     taskctl->tasks0[i].flags = 0;
     taskctl->tasks0[i].sel = (TASK_GDT0 + i) * 8;
-    set_segmdesc(gdt + TASK_GDT0 + i, 103, (int) &taskctl->tasks0[i].tss, AR_TSS32);
+    set_segmdesc(gdt + TASK_GDT0 + i, 103, (int)&taskctl->tasks0[i].tss, AR_TSS32);
   }
   for (i = 0; i < MAX_TASKLEVELS; i++) {
     taskctl->level[i].running = 0;
@@ -77,18 +82,18 @@ struct TASK *task_init(struct MEMMAN *memman) {
   }
 
   task = task_alloc();
-  task->flags = 2;	
-  task->priority = 2; 
-  task->level = 0;	
+  task->flags = 2;
+  task->priority = 2;
+  task->level = 0;
   task_add(task);
-  task_switchsub();	
+  task_switchsub();
   load_tr(task->sel);
   task_timer = timer_alloc();
   timer_settime(task_timer, task->priority);
 
   idle = task_alloc();
   idle->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
-  idle->tss.eip = (int) &task_idle;
+  idle->tss.eip = (int)&task_idle;
   idle->tss.es = 1 * 8;
   idle->tss.cs = 2 * 8;
   idle->tss.ss = 1 * 8;
@@ -100,14 +105,15 @@ struct TASK *task_init(struct MEMMAN *memman) {
   return task;
 }
 
-struct TASK *task_alloc(void) {
-  struct TASK *task;
+struct TASK* task_alloc(void)
+{
+  struct TASK* task;
   for (int i = 0; i < MAX_TASKS; i++) {
     if (taskctl->tasks0[i].flags == 0) {
       task = &taskctl->tasks0[i];
-      task->flags = 1; 
-      task->tss.eflags = 0x00000202; // IF = 1 
-      task->tss.eax = 0; 
+      task->flags = 1;
+      task->tss.eflags = 0x00000202; // IF = 1
+      task->tss.eax = 0;
       task->tss.ecx = 0;
       task->tss.edx = 0;
       task->tss.ebx = 0;
@@ -124,45 +130,48 @@ struct TASK *task_alloc(void) {
       return task;
     }
   }
-  return 0; 
+  return 0;
 }
 
-void task_run(struct TASK *task, int level, int priority) {
+void task_run(struct TASK* task, int level, int priority)
+{
   if (level < 0) {
-    level = task->level; 
+    level = task->level;
   }
   if (priority > 0) {
     task->priority = priority;
   }
 
-  if (task->flags == 2 && task->level != level) { 
-    task_remove(task); 
+  if (task->flags == 2 && task->level != level) {
+    task_remove(task);
   }
   if (task->flags != 2) {
     task->level = level;
     task_add(task);
   }
 
-  taskctl->lv_change = 1; 
+  taskctl->lv_change = 1;
   return;
 }
 
-void task_sleep(struct TASK *task) {
-  struct TASK *now_task;
+void task_sleep(struct TASK* task)
+{
+  struct TASK* now_task;
   if (task->flags == 2) {
     now_task = task_now();
-    task_remove(task); 
+    task_remove(task);
     if (task == now_task) {
       task_switchsub();
-      now_task = task_now(); 
+      now_task = task_now();
       farjmp(0, now_task->sel);
     }
   }
   return;
 }
 
-void task_switch(void) {
-  struct TASKLEVEL *tl = &taskctl->level[taskctl->now_lv];
+void task_switch(void)
+{
+  struct TASKLEVEL* tl = &taskctl->level[taskctl->now_lv];
   struct TASK *new_task, *now_task = tl->tasks[tl->now];
   tl->now++;
   if (tl->now == tl->running) {
@@ -179,4 +188,3 @@ void task_switch(void) {
   }
   return;
 }
-
