7ece3b32a058f560f201e83d3d12a42eb1730fa4
d0iasm <doiasami1219@gmail.com>
2018-12-20 17:20:35 -0800

    Adjust the window position showing up first

--- console.c
+++ console.c
@@ -349,8 +349,8 @@ int* hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
     sht->flags |= 0x10;
     sheet_setbuf(sht, (char*)ebx + ds_base, esi, edi, eax);
     make_window8((char*)ebx + ds_base, esi, edi, (char*)ecx + ds_base, 0);
-    sheet_slide(sht, 100, 50);
-    sheet_updown(sht, 3); // The height of 3 means above task_a window.
+    sheet_slide(sht, (shtctl->xsize - esi) / 2, (shtctl->ysize - edi) / 2);
+    sheet_updown(sht, shtctl->top); // The height of 3 means above task_a window.
     reg[7] = (int)sht;
   } else if (edx == 6) {
     sht = (struct SHEET*)(ebx & 0xfffffffe);
